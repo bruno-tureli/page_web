bdd579f48f1a3c03d6379092111ed57d9bd4aaee
Bruno dos santos Ventureli <brunoventureli@protonmail.com>
2026-02-05 11:18:25 -0300

    Add files via upload
    Trata-se de um experimento usando o node.js, banco de dados sql, sequelize e ejs. O intuito foi fazer uma página de perguntas e respostas parecido com o da Yahoo.

---
+++ perguntasRespostas/database/database.js
@@ -0,0 +1,10 @@
+// create modeule - conection with sequilize
+
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('guiaperguntas','root','root',{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = connection;
